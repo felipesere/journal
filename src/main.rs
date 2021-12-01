63e24f0f5c97f66f9aff8653c4879cfa7c94140a
Felipe Sere <felipesere@gmail.com>
2021-12-01 12:05:02 +0000

    Extract incomplete work items in the TODO section

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use figment::{
 use pulldown_cmark::{Event, Options, Parser as MdParser, Tag};
 use serde::Deserialize;
 use std::{ops::Range, path::PathBuf};
+use tracing::Level;
 
 use clap::{AppSettings, Parser};
 
@@ -59,51 +60,99 @@ enum ParserState {
 
 struct JournalParser {
     found_todos: Vec<Range<usize>>,
+    state: ParserState,
+}
+
+#[derive(Eq, PartialEq)]
+enum TodoHeader {
+    NotFound,
+    Found,
+    ProcessedTitle,
 }
 
 impl JournalParser {
     fn new() -> Self {
         JournalParser {
             found_todos: Vec::new(),
+            state: ParserState::Initial,
         }
     }
 
-    fn process(&mut self, markdown: &str) -> () {
+    fn process(&mut self, markdown: &str) {
         let mut options = Options::empty();
         options.insert(Options::ENABLE_TASKLISTS);
         let parser = MdParser::new_ext(markdown, options);
 
-        let mut state = ParserState::Initial;
+        let mut found_top_level_item = false;
+        let mut range_of_todo_item = None;
 
-        let mut md_iterator = parser.into_offset_iter();
+        let mut depth = 0;
+        let mut todo_header = TodoHeader::NotFound;
 
-        while let Some((event, range)) = md_iterator.next() {
-            match event {
-                Event::Start(Tag::Heading(2)) => {
-                    if let Some((heading_title, _)) = md_iterator.next() {
-                        match heading_title {
-                            Event::Text(ref t) if t.to_string() == "TODOs" => {
-                                state = ParserState::FoundTODOHeader
-                            }
-                            _ => state = ParserState::Done,
-                        }
-                    }
-                },
-                Event::Start(Tag::List(_)) if state == ParserState::FoundTODOHeader => {
-                    state = ParserState::GettingTODOs;
-                },
-                Event::End(Tag::List(None)) if state == ParserState::GettingTODOs => {
-                    state = ParserState::Done;
+        for (event, range) in parser.into_offset_iter() {
+            let span =
+                tracing::span!(Level::INFO, "processing_events", ?event, ?self.state, ?depth);
+            let _entered = span.enter();
+
+            // Pulled this out of the match statement below to make it
+            // easier to express: Found a header, and now we've processed it
+            match (&event, &todo_header) {
+                (Event::Start(Tag::Heading(2)), _) => {
+                    todo_header = TodoHeader::Found;
                 }
-                Event::Start(Tag::Item) if state == ParserState::GettingTODOs => {
-                    match md_iterator.next() {
-                        Some((Event::TaskListMarker(done), _)) if !done => {
-                            self.found_todos.push(range);
-                        },
-                        _ => {},
-                    };
-                },
-                _ => {},
+                (_, TodoHeader::ProcessedTitle) => {
+                    todo_header = TodoHeader::NotFound;
+                }
+                _ => {}
+            }
+
+            match event {
+                Event::Start(Tag::Heading(2)) => {} // handled above, prevents the catch-all at the bottom
+                Event::Text(ref text) if todo_header == TodoHeader::Found => {
+                    todo_header = TodoHeader::ProcessedTitle;
+                    if text.to_string() == "TODOs" {
+                        tracing::info!("Found a TODO header");
+                        self.state = ParserState::FoundTODOHeader;
+                    } else {
+                        tracing::info!("New section, done with TODOs");
+                        self.state = ParserState::Done;
+                    }
+                }
+                Event::Start(Tag::List(_)) if self.state == ParserState::FoundTODOHeader => {
+                    tracing::info!("Processing list within TODO header");
+                    self.state = ParserState::GettingTODOs;
+                }
+                Event::End(Tag::List(_)) => {
+                    tracing::info!("Found the end of a list");
+                }
+                Event::Start(Tag::Item)
+                    if self.state == ParserState::GettingTODOs && depth == 0 =>
+                {
+                    tracing::info!("Found the beginning of an item");
+                    depth += 1;
+                    found_top_level_item = true;
+                    range_of_todo_item = Some(range);
+                }
+                Event::Start(Tag::Item) if self.state == ParserState::GettingTODOs => {
+                    depth += 1;
+                }
+                Event::TaskListMarker(done) if found_top_level_item => {
+                    tracing::info!("Found a TODO item.");
+                    found_top_level_item = false;
+                    if !done {
+                        tracing::info!("Storing incomplete TODO item");
+                        self.found_todos.push(range_of_todo_item.take().unwrap());
+                    } else {
+                        tracing::info!("Skipping completed TODO");
+                    }
+                }
+                Event::End(Tag::Item) if self.state == ParserState::GettingTODOs => {
+                    depth -= 1;
+                    tracing::info!("End of an item");
+                }
+                _ => {
+                    tracing::info!("Ignoring event");
+                }
             }
         }
     }
@@ -118,6 +167,13 @@ fn main() {
         });
 
     let cli = Cli::parse();
+
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
     match cli.cmd {
         Cmd::New { title: _title } => {
             let markdown = include_str!("../example/full.md");
@@ -137,19 +193,102 @@ fn main() {
 #[cfg(test)]
 mod test {
     mod parsing {
-        use crate::JournalParser;
+        use crate::{JournalParser, ParserState};
         use indoc::indoc;
+        use tracing_test::traced_test;
 
         #[test]
-        fn finds_incomplete_todos() {
+        #[traced_test]
+        fn parser_knows_when_found_the_todo_header() {
+            let markdown = indoc! {r#"
+                # Something
+
+                ## TODOs
+
+                abc
+                "#};
+
+            let mut parser = JournalParser::new();
+            parser.process(markdown);
+
+            assert_eq!(parser.state, ParserState::FoundTODOHeader,);
+        }
+
+        #[test]
+        #[traced_test]
+        fn parser_knows_when_it_is_looking_at_a_todo_list() {
+            let markdown = indoc! {r#"
+                # Something
+
+                ## TODOs
+
+                * [ ] abc
+                "#};
+
+            let mut parser = JournalParser::new();
+            parser.process(markdown);
+
+            assert_eq!(parser.state, ParserState::GettingTODOs);
+            assert_eq!(parser.found_todos.len(), 1);
+        }
+
+        #[test]
+        #[traced_test]
+        fn parser_knows_when_its_done_with_todos() {
+            let markdown = indoc! {r#"
+                # Something
+
+                ## TODOs
+
+                ## Not TODOs
+
+                "#};
+
+            let mut parser = JournalParser::new();
+            parser.process(markdown);
+
+            assert_eq!(parser.state, ParserState::Done);
+            assert_eq!(parser.found_todos.len(), 0);
+        }
+
+        #[test]
+        #[traced_test]
+        fn finds_multiple_todos() {
             let markdown = indoc! {r#"
                 # Something
 
                 ## TODOs
 
                 * [ ] first
-                  * [ ] middle
-                  * Random text!
+
+                * [ ] second
+
+                * [ ] third
+
+                ## Other thing
+                "#};
+
+            let mut parser = JournalParser::new();
+            parser.process(markdown);
+
+            for todo in &parser.found_todos {
+                println!("---------------");
+                println!("{}", &markdown[todo.start..todo.end]);
+                println!("---------------");
+            }
+
+            assert_eq!(parser.found_todos.len(), 3);
+        }
+
+        #[test]
+        #[traced_test]
+        fn skips_completed_todos() {
+            let markdown = indoc! {r#"
+                # Something
+
+                ## TODOs
+
+                * [ ] first
 
                 * [x] second
 
@@ -160,6 +299,72 @@ mod test {
 
             let mut parser = JournalParser::new();
             parser.process(markdown);
+
+            for todo in &parser.found_todos {
+                println!("---------------");
+                println!("{}", &markdown[todo.start..todo.end]);
+                println!("---------------");
+            }
+
+            assert_eq!(parser.found_todos.len(), 2);
+        }
+
+        #[test]
+        #[traced_test]
+        fn ignores_todos_beneath_a_completed_one() {
+            let markdown = indoc! {r#"
+                # Something
+
+                ## TODOs
+
+                * [ ] first
+
+                * [x] second
+                    * [ ] second.dot.one
+
+                * [ ] third
+
+                ## Other thing
+                "#};
+
+            let mut parser = JournalParser::new();
+            parser.process(markdown);
+
+            for todo in &parser.found_todos {
+                println!("---------------");
+                println!("{}", &markdown[todo.start..todo.end]);
+                println!("---------------");
+            }
+
+            assert_eq!(parser.found_todos.len(), 2);
+        }
+
+        #[test]
+        #[traced_test]
+        fn ignores_normal_bullet_lists_within_completed_ones() {
+            let markdown = indoc! {r#"
+                # Something
+
+                ## TODOs
+
+                * [ ] first
+
+                * [x] second
+                    * second.dot.one
+
+                * [ ] third
+
+                ## Other thing
+                "#};
+
+            let mut parser = JournalParser::new();
+            parser.process(markdown);
+
+            for todo in &parser.found_todos {
+                println!("---------------");
+                println!("{}", &markdown[todo.start..todo.end]);
+                println!("---------------");
+            }
 
             assert_eq!(parser.found_todos.len(), 2);
         }
