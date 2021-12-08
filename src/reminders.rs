ceddf6daefec7ab0d2262d942aa592533f76a2bb
Felipe Sere <felipesere@gmail.com>
2021-12-08 16:58:38 +0000

    Use custom parser instead of relying on time-rs and its complex machinery
    REBASEME

--- src/reminders.rs
+++ src/reminders.rs
@@ -1,11 +1,12 @@
 use std::collections::BTreeMap;
 use std::num::ParseIntError;
+use std::ops::Mul;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 
 use anyhow::{anyhow, Context, Result};
 use serde::{Deserialize, Serialize};
-use time::{format_description, Date, Month, OffsetDateTime, Weekday};
+use time::{Date, Month, OffsetDateTime, Weekday};
 
 pub trait Clock {
     fn today(&self) -> Date;
@@ -27,19 +28,28 @@ pub struct ReminderConfig {
 #[derive(Deserialize, Serialize)]
 pub struct Reminders {
     dated: BTreeMap<Date, Vec<String>>,
+    intervals: Vec<RepeatingReminder>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct RepeatingReminder {
+    start: Date,
+    interval: RepeatingDate,
+    reminder: String,
 }
 
 impl Reminders {
     pub fn new() -> Self {
         Self {
             dated: BTreeMap::new(),
+            intervals: Vec::new(),
         }
     }
 
     pub fn load(path: &Path) -> Result<Self> {
         let content = std::fs::read(path)
             .with_context(|| format!("Could not load reminders from {:?}", path))?;
-        serde_json::from_slice(&content).map_err(|e| anyhow!(e))
+        serde_json::from_slice(&content).map_err(|e| anyhow!(e)).context("Could not read structure in file")
     }
 
     pub fn save(&self, path: &Path) -> Result<()> {
@@ -59,11 +69,41 @@ impl Reminders {
     pub fn for_today(&self, clock: &impl Clock) -> Vec<String> {
         let today = clock.today();
 
-        if let Some(reminders) = self.dated.get(&today) {
-            reminders.to_vec()
-        } else {
-            Vec::new()
+        let mut reminders = Vec::new();
+
+        if let Some(dated_reminders) = self.dated.get(&today) {
+            reminders.extend_from_slice(dated_reminders)
         }
+
+        for repeating_reminder in &self.intervals {
+            match &repeating_reminder.interval {
+                RepeatingDate::Weekday(weekday) => {
+                    if today.weekday() == *weekday {
+                        reminders.push(repeating_reminder.reminder.clone());
+                    }
+                }
+                RepeatingDate::Periodic { amount, period } => {
+                    let interval_in_days = amount * period;
+                    let difference =
+                        today.to_julian_day() - repeating_reminder.start.to_julian_day();
+
+                    if difference % interval_in_days == 0 {
+                        reminders.push(repeating_reminder.reminder.clone());
+                    }
+                }
+            }
+        }
+
+        reminders
+    }
+
+    pub fn every(&mut self, clock: &impl Clock, interval: RepeatingDate, reminder: &str) {
+        let start = clock.today();
+        self.intervals.push(RepeatingReminder {
+            start,
+            interval,
+            reminder: reminder.to_string(),
+        });
     }
 }
 
@@ -99,99 +139,108 @@ impl FromStr for SpecificDate {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if let Ok(result) = parse_day_month_year(s) {
-            return Ok(result);
-        }
+        let components: Vec<&str> = s.split('.').collect();
 
-        if let Ok(result) = parse_day_month(s) {
-            return Ok(result);
+        match &components[..] {
+            [day, month, year] => {
+                let day: u8 = str::parse(day).map_err(|e: ParseIntError| e.to_string())?;
+                let month = parse_month(month)?;
+                let year: i32 = str::parse(year).map_err(|e: ParseIntError| e.to_string())?;
+                Ok(SpecificDate::OnDate(
+                    Date::from_calendar_date(year, month, day).map_err(|e| e.to_string())?,
+                ))
+            }
+            [day, month] => {
+                let day: u8 = str::parse(day).map_err(|e: ParseIntError| e.to_string())?;
+                let month = parse_month(month)?;
+                Ok(SpecificDate::OnDayMonth(day, month))
+            }
+            [weekday] => {
+                let weekday = parse_weekday(weekday)?;
+                Ok(SpecificDate::Next(weekday))
+            }
+            _ => Err(
+                "No matching date format found. Use day.month or day.monty.year or weekday.".to_string(),
+            ),
         }
-
-        parse_weekday(s).map(|day| SpecificDate::Next(day))
     }
 }
 
-fn parse_day_month_year(s: &str) -> Result<SpecificDate, String> {
-    let format = format_description::parse("[day].[month repr:short].[year]").unwrap();
-
-    let date = Date::parse(s, &format).map_err(|e| e.to_string())?;
-
-    Ok(SpecificDate::OnDate(date))
-}
-
+#[rustfmt::skip]
 fn parse_weekday(s: &str) -> Result<Weekday, String> {
     let day = match s {
-        "Monday" | "monday" => Weekday::Monday,
-        "Tuesday" | "tuesday" => Weekday::Tuesday,
+        "Monday"    | "monday"   => Weekday::Monday,
+        "Tuesday"   | "tuesday"  => Weekday::Tuesday,
         "Wednesday" | "wedneday" => Weekday::Wednesday,
-        "Thursday" | "thursday" => Weekday::Thursday,
-        "Friday" | "friday" => Weekday::Friday,
-        "Saturday" | "saturday" => Weekday::Saturday,
-        "Sunday" | "sunday" => Weekday::Sunday,
+        "Thursday"  | "thursday" => Weekday::Thursday,
+        "Friday"    | "friday"   => Weekday::Friday,
+        "Saturday"  | "saturday" => Weekday::Saturday,
+        "Sunday"    | "sunday"   => Weekday::Sunday,
         _ => return Err(format!("No matching day of the week: {}", s)),
     };
 
     Ok(day)
 }
 
-fn parse_day_month(s: &str) -> Result<SpecificDate, String> {
-    use time::format_description::modifier::{Day, Month, MonthRepr, Padding};
-    use time::format_description::Component;
-    use time::format_description::FormatItem::*;
+#[rustfmt::skip]
+fn parse_month(month: &str) -> Result<Month, String> {
+    let month = match month {
+        "January"   | "Jan" | "january"   | "jan" => Month::January,
+        "February"  | "Feb" | "february"  | "feb" => Month::February,
+        "March"     | "Mar" | "march"     | "mar" => Month::March,
+        "April"     | "Apr" | "april"     | "apr" => Month::April,
+        "May"                             | "may" => Month::May,
+        "June"      | "Jun" | "june"      | "jun" => Month::June,
+        "July"      | "Jul" | "july"      | "jul" => Month::July,
+        "August"    | "Aug" | "august"    | "aug" => Month::August,
+        "September" | "Sep" | "september" | "sep" => Month::September,
+        "October"   | "Oct" | "october"   | "oct" => Month::October,
+        "November"  | "Nov" | "november"  | "nov" => Month::November,
+        "December"  | "Dec" | "december"  | "dec" => Month::December,
+        _ => return Err(format!("No matching month name: {}", month)),
+    };
 
-    let mut month = Month::default();
-    month.repr = MonthRepr::Short;
-    month.padding = Padding::None;
-    month.case_sensitive = false;
-
-    let mut day = Day::default();
-    day.padding = Padding::None;
-
-    let mut parsed = time::parsing::Parsed::new();
-    let structure_to_parse = vec![
-        Component(Component::Day(day)),
-        Literal(&[b'.']),
-        Component(Component::Month(month)),
-    ];
-
-    parsed
-        .parse_items(s.as_bytes(), &structure_to_parse)
-        .map_err(|e| e.to_string())?;
-
-    let day = parsed
-        .day()
-        .ok_or_else(|| format!("Could get month component from '{}'", &s))?;
-    let month = parsed
-        .month()
-        .ok_or_else(|| format!("Could get date from '{}'", &s))?;
-
-    Ok(SpecificDate::OnDayMonth(day.get(), month))
+    Ok(month)
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub enum RepeatingDate {
     Weekday(Weekday),
-    Periodic(usize, Period),
+    Periodic { amount: usize, period: Period },
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
 pub enum Period {
     Days,
     Weeks,
     Months,
 }
 
+impl Mul<&Period> for &usize {
+    type Output = i32;
+
+    fn mul(self, rhs: &Period) -> Self::Output {
+        let rhs = match rhs {
+            Period::Days => 1,
+            Period::Weeks => 7,
+            Period::Months => 30, // TODO: indicator that is clearly not OK with months...
+        };
+
+        (*self as i32) * rhs
+    }
+}
+
 impl FromStr for RepeatingDate {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parsed = parse_weekday(s).map(|day| RepeatingDate::Weekday(day));
+        let parsed = parse_weekday(s).map(RepeatingDate::Weekday);
         if parsed.is_ok() {
             return parsed;
         }
 
         if let Some((digits, period)) = s.split_once('.') {
-            let amount = str::parse(&digits).map_err(|e: ParseIntError| e.to_string())?;
+            let amount = str::parse(digits).map_err(|e: ParseIntError| e.to_string())?;
             let period = match period {
                 "days" => Period::Days,
                 "weeks" => Period::Weeks,
@@ -199,7 +248,7 @@ impl FromStr for RepeatingDate {
                 _ => return Err(format!("unknown period: {}", period)),
             };
 
-            return Ok(RepeatingDate::Periodic(amount, period));
+            return Ok(RepeatingDate::Periodic { amount, period });
         }
 
         Err(format!("Unrecognized format for repeating date: {}", s))
@@ -241,6 +290,15 @@ mod tests {
         pub(crate) fn advance_by(&mut self, days: Duration) {
             self.current_date = self.current_date.add(days);
         }
+
+        pub(crate) fn advance_to(&mut self, weekday: Weekday) {
+            loop {
+                if self.current_date.weekday() == weekday {
+                    break;
+                }
+                self.current_date = self.current_date.next_day().unwrap();
+            }
+        }
     }
 
     #[test]
@@ -266,12 +324,46 @@ mod tests {
     }
 
     #[test]
+    fn repeating_reminders() -> Result<()> {
+        use time::Weekday::*;
+        let mut clock = ControlledClock::new(2021, July, 15)?;
+        let mut reminders = Reminders::new();
+
+        clock.advance_to(Monday);
+        reminders.every(&clock, RepeatingDate::Weekday(Wednesday), "Email someone");
+
+        clock.advance_to(Wednesday);
+        let todays_reminders = reminders.for_today(&clock);
+        assert_eq!(todays_reminders, vec!["Email someone".to_string()]);
+
+        clock.advance_by(1.days()); // Thursday
+        reminders.every(
+            &clock,
+            RepeatingDate::Periodic {
+                amount: 2,
+                period: Period::Weeks,
+            },
+            "Second task",
+        );
+
+        clock.advance_by(1.weeks()); // next Thursday
+        let todays_reminders = reminders.for_today(&clock);
+        assert!(todays_reminders.is_empty());
+
+        clock.advance_by(1.weeks()); // Thursday after that...
+        let todays_reminders = reminders.for_today(&clock);
+        assert_eq!(todays_reminders, vec!["Second task".to_string()]);
+
+        Ok(())
+    }
+
+    #[test]
     fn adding_multiple_reminders_on_filesystem() -> Result<()> {
         let mut clock = ControlledClock::new(2021, July, 15)?;
 
         let dir = TempDir::new().unwrap();
         dir.child("reminders.json")
-            .write_str(r#"{"dated": {}}"#)
+            .write_str(r#"{"dated": {}, "intervals": [] }"#)
             .unwrap();
 
         let mut reminders = Reminders::load(&dir.path().join("reminders.json"))?;
@@ -327,7 +419,7 @@ mod tests {
         use super::*;
         use data_test::data_test;
         use std::str::FromStr;
-        use time::{macros::date, Weekday};
+        use time::Weekday;
 
         data_test! {
 
@@ -337,9 +429,9 @@ mod tests {
                 assert_eq!(RepeatingDate::from_str(input), expected);
             }
             - weekday ("Wednesday", Ok(super::RepeatingDate::Weekday(super::Weekday::Wednesday)))
-            - n_days ("2.days", Ok(super::RepeatingDate::Periodic(2, super::Period::Days)))
-            - n_weeks ("7.weeks", Ok(super::RepeatingDate::Periodic(7, super::Period::Weeks)))
-            - n_months ("3.months", Ok(super::RepeatingDate::Periodic(3, super::Period::Months)))
+            - n_days ("2.days", Ok(super::RepeatingDate::Periodic{amount: 2, period: super::Period::Days}))
+            - n_weeks ("7.weeks", Ok(super::RepeatingDate::Periodic{amount: 7, period: super::Period::Weeks}))
+            - n_months ("3.months", Ok(super::RepeatingDate::Periodic{amount: 3, period: super::Period::Months}))
             - negative_amount ("-1.months", Err("invalid digit found in string".into()))
             - unknown_period ("1.fortnights", Err("unknown period: fortnights".into()))
             - missing_separator ("quaselgoop", Err("Unrecognized format for repeating date: quaselgoop".into()))
@@ -364,33 +456,37 @@ mod tests {
         }
     }
 
-    #[test]
-    fn specifics_dates_are_their_own_next_date() {
-        let jan_15_2022 = date!(2022 - 01 - 15);
-        let specific_date = SpecificDate::OnDate(jan_15_2022.clone());
+    mod specific_date {
+        use super::*;
 
-        let next_date = specific_date.next_date(date!(2022 - 01 - 10));
+        #[test]
+        fn specifics_dates_are_their_own_next_date() {
+            let jan_15_2022 = date!(2022 - 01 - 15);
+            let specific_date = SpecificDate::OnDate(jan_15_2022.clone());
 
-        assert_eq!(jan_15_2022, next_date);
-    }
+            let next_date = specific_date.next_date(date!(2022 - 01 - 10));
 
-    #[test]
-    fn day_month_dates_use_year_of_item_if_possible() {
-        let specific_date = SpecificDate::OnDayMonth(9, Month::December);
+            assert_eq!(jan_15_2022, next_date);
+        }
 
-        let dez_7_2021 = date!(2021 - 12 - 07);
-        let next_date = specific_date.next_date(dez_7_2021);
+        #[test]
+        fn day_month_dates_use_year_of_item_if_possible() {
+            let specific_date = SpecificDate::OnDayMonth(9, Month::December);
 
-        assert_eq!(date!(2021 - 12 - 09), next_date);
-    }
+            let dez_7_2021 = date!(2021 - 12 - 07);
+            let next_date = specific_date.next_date(dez_7_2021);
 
-    #[test]
-    fn weekday_picks_next_available_weekday() {
-        let specific_date = SpecificDate::Next(Weekday::Wednesday);
+            assert_eq!(date!(2021 - 12 - 09), next_date);
+        }
 
-        let dez_7_2021 = date!(2021 - 12 - 07);
-        let next_date = specific_date.next_date(dez_7_2021);
+        #[test]
+        fn weekday_picks_next_available_weekday() {
+            let specific_date = SpecificDate::Next(Weekday::Wednesday);
 
-        assert_eq!(date!(2021 - 12 - 08), next_date);
+            let dez_7_2021 = date!(2021 - 12 - 07);
+            let next_date = specific_date.next_date(dez_7_2021);
+
+            assert_eq!(date!(2021 - 12 - 08), next_date);
+        }
     }
 }
